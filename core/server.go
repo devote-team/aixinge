c3a54d4955a1194019bcd86dbaae1455f44dfcc7
hubin <243194995@qq.com>
2022-11-19 08:40:22 +0800

    feat: kill daemon exit, app shutdown

--- core/server.go
+++ core/server.go
@@ -5,6 +5,10 @@ import (
 	"aixinge/initialize"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -13,11 +17,25 @@ type Server interface {
 }
 
 func RunServer() {
-	Router := initialize.Routers()
+	// init routers
+	app := initialize.Routers()
 	address := fmt.Sprintf(":%d", global.CONFIG.System.Port)
 
+	// kill daemon exit
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		<-quit
+		fmt.Println("Shutdown Server ...")
+		if err := app.Shutdown(); err != nil {
+			fmt.Println(err)
+			log.Fatalf("Server Shutdown: %s", err)
+		}
+		fmt.Println("Server exit")
+	}()
+
 	time.Sleep(10 * time.Microsecond)
-	global.LOG.Error(newServer().ServeAsync(address, Router).Error())
+	global.LOG.Error(newServer().ServeAsync(address, app).Error())
 }
 
 func newServer() Server {
