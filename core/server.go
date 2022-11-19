2e27968c089c0cec4196d4e72e7d224872429431
hubin <243194995@qq.com>
2022-11-19 08:43:10 +0800

    恢复

--- core/server.go
+++ core/server.go
@@ -5,10 +5,6 @@ import (
 	"aixinge/initialize"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
-	"log"
-	"os"
-	"os/signal"
-	"syscall"
 	"time"
 )
 
@@ -20,19 +16,6 @@ func RunServer() {
 	// init routers
 	app := initialize.Routers()
 	address := fmt.Sprintf(":%d", global.CONFIG.System.Port)
-
-	// kill daemon exit
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-	go func() {
-		<-quit
-		fmt.Println("Shutdown Server ...")
-		if err := app.Shutdown(); err != nil {
-			fmt.Println(err)
-			log.Fatalf("Server Shutdown: %s", err)
-		}
-		fmt.Println("Server exit")
-	}()
 
 	time.Sleep(10 * time.Microsecond)
 	global.LOG.Error(newServer().ServeAsync(address, app).Error())
